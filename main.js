9f3abfc84b390e809e8f42728b813df83ef4540f
Alisson Melo <alissonmelo.work@gmail.com>
2021-08-22 02:33:30 -0300

    create the logic of the piano

---
+++ main.js
@@ -0,0 +1,57 @@
+// get all keys
+const keys = document.querySelectorAll('.key')
+
+// play notes
+const playNote = event => {
+  let audioKeyCode = getKeyCode(event)
+
+  // type or pressed key
+  const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+
+  // if key exist
+  const cantFoundAndKey = !key
+  if (cantFoundAndKey) {
+    return
+  }
+
+  // play audio
+  addPlayingClass(key)
+  playAudio(audioKeyCode)
+}
+
+function getKeyCode(event) {
+  // keyCode
+  let keyCode
+
+  const isKeyboard = event.type === 'keydown'
+  if (isKeyboard) {
+    keyCode = event.keyCode
+  } else {
+    keyCode = event.target.dataset.key
+  }
+
+  return keyCode
+}
+
+function playAudio(audioKeyCode) {
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+  audio.currentTime = 0
+  audio.play()
+}
+
+function addPlayingClass(key) {
+  key.classList.add('playing')
+}
+
+function removePlayingClass(event) {
+  event.target.classList.remove('playing')
+}
+
+// click with mouse
+keys.forEach(key => {
+  key.addEventListener('click', playNote)
+  key.addEventListener('transitionend', removePlayingClass)
+})
+
+// keyboard type
+window.addEventListener('keydown', playNote)
